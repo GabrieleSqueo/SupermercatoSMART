7dcb92e45cee5401d8ffe2771c70a5d7eb647d63
GabrieleSqueo <gsqueo3@gmail.com>
2025-07-12 11:42:53 +0200

    Update Productslist.tsx

--- app/components/dashboardComponents/Productslist.tsx
+++ app/components/dashboardComponents/Productslist.tsx
@@ -26,8 +26,9 @@ const Productslist = () => {
   }, []); 
   return (
     <div className="w-full">
-      <div className="grid grid-cols-1 md:grid-cols-2 gap-8 mb-8">
-        <h2 className="text-2xl font-bold text-blue-700 mb-4">Prodotti Disponibili</h2>
+      
+      <h2 className="flex text-2xl font-bold text-blue-700 mb-6 justify-center">Lista Prodotti</h2>  
+      <div className="space-y-6">
         {prodotti && prodotti.map((prod) => <ProductItem prodotto={prod} /> ) }
       </div>
     </div>
